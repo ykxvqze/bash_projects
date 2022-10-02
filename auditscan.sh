ab12065aac797b62504e71a11d3def4a5ac4f679
thln2ejz <xrzfyvqk_k1jw@protonmail.ch>
2022-10-02 21:11:54 +0300

    updated auditscan.sh

--- auditscan.sh
+++ auditscan.sh
@@ -7,9 +7,21 @@ setcolor() {
     blu='\e[34m'
 }
 
+audit_report="$PWD/audit_report_`date +'%Y_%m_%d_%H%M%S'`"
+
+printf "%s%s\n" "Operating System         : `uname -s`"                         >> "${audit_report}"
+printf "%s%s\n" "Operating System Name    : `lsb_release -i | sed 's/.*:\s//'`" >> "${audit_report}"
+printf "%s%s\n" "Operating System Version : `lsb_release -d | sed 's/.*:\s//'`" >> "${audit_report}"
+printf "%s%s\n" "Kernel Version           : `uname -r`"                         >> "${audit_report}"
+printf "%s%s\n" "Hardware Platform        : `uname -m`"                         >> "${audit_report}"
+printf "%s%s\n" "Hostname                 : `hostname`"                         >> "${audit_report}"
+
 filetmp='/tmp/config.json'
 keys=`jq -r 'keys[]' audit.json`
 setcolor
+
+n_pass=0
+n_fail=0
 
 for key in $keys; do
     sed -n "/\"${key////\\/}\"/,/\s]/p" audit.json |
@@ -36,10 +48,40 @@ for key in $keys; do
         printf '%s\n' "[*] $title"
         eval "$audit" &> /dev/null
         if [ "`eval "$audit" 2>/dev/null`" != "$expected" ]; then
-            printf "${red}%s${rst}\n" "[!] Audit step  : $audit"
+            ((n_fail+=1))
+
+            # stdout
+            printf "${red}%s${rst}\n" "[X] Audit step  : $audit"
             printf "${red}%s${rst}\n" "[-] Remediation : $remediation"
+
+            # report
+            printf "\n%s%s\n" "/!\ [FAIL]" "[*] $title"                     >> "${audit_report}"
+            printf "%s%s\n  " "          " "[X] Audit step  : $audit"       >> "${audit_report}"
+            printf "%s%s\n  " "          " "[-] Remediation : $remediation" >> "${audit_report}"
         else
-            printf "${grn}%s${rst}\n" "[x] Audit step  : $audit"
+            ((n_pass+=1))
+
+            # stdout
+            printf "${grn}%s${rst}\n" "[V] Audit step  : $audit"
+
+            # report
+            printf "\n%s%s\n" "    [PASS]" "[*] $title"                   >> "${audit_report}"
+            printf "%s%s\n  " "          " "[V] Audit step  : $audit"     >> "${audit_report}"
         fi
      done
 done
+
+echo
+printf "${rst}%s\n" "=================================================="
+printf "${rst}%s${rst}\n" "Audit complete."
+echo
+
+printf "${rst}%s${rst}\n" "SUMMARY"
+echo '-------'
+printf "${grn}%s${grn}\n" "PASS: ${n_pass}"
+printf "${red}%s${red}\n" "FAIL: ${n_fail}"
+echo
+
+printf "${rst}%s${rst}\n" "Audit file saved in current directory."
+echo "Filename: ${audit_report}"
+echo
