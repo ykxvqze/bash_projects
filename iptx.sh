ddc9c941940c27efe4f2b21e9c735c5f6efa907c
thln2ejz <ykxvqz@pm.me>
2023-10-07 13:40:34 +0300

    refactoring

--- iptx.sh
+++ iptx.sh
@@ -2,7 +2,7 @@
 << 'EOF'
 Utility functions for IPv4 networking/transformations
 
-USAGE: . ./iptx.sh --source-only
+USAGE: . ./iptx.sh
        cidr2network 128.42.5.4/21
        cidr2broadcast 128.42.5.4/21
 
@@ -10,10 +10,9 @@ USAGE: . ./iptx.sh --source-only
 
 OPTIONS:
        [ -h ]              Print usage
-       [ --source-only ]   Source the script without executing main()
 
 EXAMPLES:
-         . ./iptx.sh --source-only
+         . ./iptx.sh                   # source the script
          cidr2network 128.42.5.4/21    # get network address
          cidr2netmask 128.42.5.4/21    # get netmask
          cidr2broadcast 128.42.5.4/21  # get broadcast address
@@ -39,7 +38,7 @@ DESCRIPTION:
 EXAMPLES:
 
 ip2bin 128.42.5.4                             # 10000000 00101010 00000101 00000100
-bin2ip '10000000 00101010 00000101 00000100'  # 128.42.5.4
+bin2ip '10000000;00101010;00000101;00000100'  # 128.42.5.4
 cidr2network 128.42.5.4/21                    # 128.42.0.0
 cidr2netmask 128.42.5.4/21                    # 255.255.248.0
 cidr2broadcast 128.42.5.4/21                  # 128.42.7.255
@@ -71,163 +70,151 @@ cidr2numhosts  () { :; }  # CIDR to number of hosts
 cidr2ipfirst   () { :; }  # CIDR to first usable IP address
 cidr2iplast    () { :; }  # CIDR to last usable IP address
 
-print_usage(){
+print_usage() {
     echo -e "iptx.sh: utility for CIDR to IPv4 transformations.
     Usage:
            ./${0##*/} 128.42.5.4/21
 
-           . ./${0##*/} --source-only
+           . ./${0##*/}
            cidr2network 128.42.5.4/21
            cidr2broadcast 128.42.5.4/21"
 }
 
 valid_ipv4() {
-    local ip="$1"
-    grep -Eo '^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$' <<< "$ip" &> /dev/null
+    local ip="${1}"
+    [[ "${ip}" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]
     return "$?"
 }
 
 valid_cidr() {
-    local ip_cidr="$1"
+    local ip_cidr="${1}"
     local status=1
-
-    grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}/([1-9]|[1-2][0-9]|3[0-1])$' <<< "$ip_cidr" &> /dev/null
-    if [ "$?" -eq 0 ]; then
+    if [[ "${ip_cidr}" =~ ^[^/]*/([0-9]|[1-2][0-9]|3[0-2])$ ]]; then
         ip=$(echo "${ip_cidr}" | cut -d '/' -f 1)
-        valid_ipv4 "$ip"
-        if [ "$?" -eq 0 ]; then
-            status=0
-        fi
+        netmask_length=$(echo "${ip_cidr}" | cut -d '/' -f 2)
+        valid_ipv4 "${ip}" && status=0
     fi 
-    return "$status"
+    return "${status}"
 }
 
 # Example: 110 --> 00000110
-zero_pad(){
-    local input="$1"
+zero_pad() {
+    local input="${1}"
     local z
-    n=`expr length "$input"`
+    n="$(expr length "${input}")"
 
-    if [ "$n" -lt 8 ]; then
+    if [ "${n}" -lt 8 ]; then
 	    d=$((8-n))
-        z=`head -c "$d" /dev/zero | tr '\0' '0'`
+        z="$(head -c "$d" /dev/zero | tr '\0' '0')"
     fi
     echo "${z}${input}"
 }
 
 # Example: 128.42.5.4 --> 10000000 00101010 00000101 00000100
 ip2bin() {
-    local ip="$1"
-    x=$(echo "$ip" | tr '.' ';')
-    r=$(echo "obase=2; ibase=10; $x" | bc)
-    for i in $r; do zero_pad "$i"; done | xargs
+    local ip="${1}"
+    x="$(echo "${ip}" | tr '.' ';')"
+    r="$(echo "obase=2; ibase=10; ${x}" | bc)"
+    for i in $r; do zero_pad "${i}"; done | xargs
 }
 
 # Example: '10000000 00101010 00000101 00000100' --> 128.42.5.4
 bin2ip() {
-    local b="$1"
-    for i in $b; do
-        echo "obase=10; ibase=2; $i" | bc
-    done | xargs | tr ' ' '.'
+    local binary_sequence="${1}"
+    echo "obase=10; ibase=2; ${binary_sequence}" | bc | xargs | tr ' ' '.'
 }
 
 # Example: 128.42.5.4/21 --> 255.255.248.0
-cidr2netmask(){
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        return 1
-    fi
-    netmask_length=`echo "${ip_cidr}" | cut -d '/' -f 2`
-    d=$((32-netmask_length))
-    ones=$(head -c "${netmask_length}" /dev/zero | tr '\0' '1')
-    zeros=$(head -c "$d" /dev/zero | tr '\0' '0')
-    sequence=`sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1 \2 \3 \4/' <<< "${ones}${zeros}"`
-    bin2ip "$sequence"
+cidr2netmask() {
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || return 1
+    netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
+    host_length="$((32-netmask_length))"
+    ones="$(head -c "${netmask_length}" /dev/zero | tr '\0' '1')"
+    zeros="$(head -c "${host_length}" /dev/zero | tr '\0' '0')"
+    sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${ones}${zeros}")"
+    bin2ip "${sequence}"
 }
 
 # Example: 128.42.5.4/21 -->  128.42.0.0
-cidr2network(){
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        return 1
-    fi
-    local netmask_length=`echo "${ip_cidr}" | cut -d '/' -f 2`
-    local ip=`echo "${ip_cidr}" | cut -d '/' -f 1`
-    local ip_bin=`ip2bin "$ip" | tr -d ' '`
+cidr2network() {
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || return 1
+    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
+    local ip="$(echo "${ip_cidr}" | cut -d '/' -f 1)"
+    local ip_bin="$(ip2bin "$ip" | tr -d ' ')"
 
-    network_part=`echo "${ip_bin:0:netmask_length}"`
-    d=$((32-netmask_length))
-    zeros=`head -c "$d" /dev/zero | tr '\0' '0'`
-    sequence=`sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1 \2 \3 \4/' <<< "${network_part}${zeros}"`
-    bin2ip "$sequence"
+    network_part="$(echo "${ip_bin:0:netmask_length}")"
+    host_length="$((32-netmask_length))"
+    zeros="$(head -c "${host_length}" /dev/zero | tr '\0' '0')"
+    sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${zeros}")"
+    bin2ip "${sequence}"
 }
 
 # Example: 128.42.5.4/21 --> 128.42.7.255
-cidr2broadcast(){
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        return 1
-    fi
-    local netmask_length=`echo "${ip_cidr}" | cut -d '/' -f 2`
-    local ip=`echo ${ip_cidr} | cut -d '/' -f 1`
-    local ip_bin=`ip2bin "$ip" | tr -d ' '`
+cidr2broadcast() {
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || return 1
+    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
+    local ip="$(echo ${ip_cidr} | cut -d '/' -f 1)"
+    local ip_bin="$(ip2bin "${ip}" | tr -d ' ')"
 
-    network_part=`echo "${ip_bin:0:netmask_length}"`
-    d=$((32-netmask_length))
-    ones=`head -c "$d" /dev/zero | tr '\0' '1'`
-    sequence=`sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1 \2 \3 \4/' <<< "${network_part}${ones}"`
-    bin2ip "$sequence"
+    network_part="$(echo "${ip_bin:0:netmask_length}")"
+    host_length="$((32-netmask_length))"
+    ones="$(head -c "${host_length}" /dev/zero | tr '\0' '1')"
+    sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${ones}")"
+    bin2ip "${sequence}"
 }
 
 # Example: 128.42.5.4/21 --> 2046 (number of usable hosts)
-cidr2numhosts(){
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        return 1
+cidr2numhosts() {
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || return 1
+    netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
+    host_length="$((32-netmask_length))"
+    if [ "${host_length}" -eq 0 ]; then
+        echo '0'
+    else 
+        echo "$((2**host_length-2))"
     fi
-    local netmask_length=`echo "${ip_cidr}" | cut -d '/' -f 2`
-    d=$((32-netmask_length))
-    echo $((2**d-2))
 }
 
 # Example: 128.42.5.4/21 --> 128.42.0.1 (first usable IP address)
-cidr2ipfirst(){
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        return 1
-    fi
-    local netmask_length=`echo "${ip_cidr}" | cut -d '/' -f 2`
-    local ip=`echo "${ip_cidr}" | cut -d '/' -f 1`
-    local ip_bin=`ip2bin "$ip" | tr -d ' '`
+cidr2ipfirst() {
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || return 1
+    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
+    local ip="$(echo "${ip_cidr}" | cut -d '/' -f 1)"
+    local ip_bin="$(ip2bin "${ip}" | tr -d ' ')"
 
-    network_part=`echo "${ip_bin:0:netmask_length}"`
-    d=$((32-netmask_length))
-    zeros=`head -c "$((d-1))" /dev/zero | tr '\0' '0'`
-    sequence=`sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1 \2 \3 \4/' <<< "${network_part}${zeros}1"`
-    bin2ip "$sequence"
+    network_part="$(echo "${ip_bin:0:netmask_length}")"
+    host_length="$((32-netmask_length))"
+    if [ "${host_length}" -le 1 ]; then
+        echo 'None'
+    else 
+        zeros="$(head -c "$((host_length-1))" /dev/zero | tr '\0' '0')"
+        sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${zeros}1")"
+        bin2ip "${sequence}"
+    fi
 }
 
 # Example: 128.42.5.4/21 --> 128.42.7.254 (last usable IP address)
-cidr2iplast(){
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        return 1
-    fi
-    local netmask_length=`echo "${ip_cidr}" | cut -d '/' -f 2`
-    local ip=`echo ${ip_cidr} | cut -d '/' -f 1`
-    local ip_bin=`ip2bin "$ip" | tr -d ' '`
+cidr2iplast() {
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || return 1
+    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
+    local ip="$(echo ${ip_cidr} | cut -d '/' -f 1)"
+    local ip_bin="$(ip2bin "$ip" | tr -d ' ')"
 
-    network_part=`echo "${ip_bin:0:netmask_length}"`
-    d=$((32-netmask_length))
-    ones=`head -c "$((d-1))" /dev/zero | tr '\0' '1'`
-    sequence=`sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1 \2 \3 \4/' <<< "${network_part}${ones}0"`
-    bin2ip "$sequence"
+    network_part="$(echo "${ip_bin:0:netmask_length}")"
+    host_length="$((32-netmask_length))"
+    if [ "${host_length}" -le 1 ]; then
+        echo 'None'
+    else 
+        ones="$(head -c "$((host_length-1))" /dev/zero | tr '\0' '1')"
+        sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${ones}0")"
+        bin2ip "${sequence}"
+    fi
 }
 
 main(){
@@ -238,12 +225,8 @@ main(){
         esac
     done
 
-    local ip_cidr="$1"
-    valid_cidr "$ip_cidr"
-    if [ "$?" -ne 0 ]; then
-        echo 'Not a valid CIDR notation'
-        exit 1
-    fi
+    local ip_cidr="${1}"
+    valid_cidr "${ip_cidr}" || exit 1
     {
     echo "Network address: $(cidr2network ${ip_cidr})"
     echo "Broadcast address: $(cidr2broadcast ${ip_cidr})"
@@ -254,6 +237,6 @@ main(){
     } | column -t -s ':'
 }
 
-if [ "${1}" != "--source-only" ]; then
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     main "${@}"
 fi
