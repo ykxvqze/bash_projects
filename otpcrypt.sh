f2fe304a507e00d1a9443551b5bf1e8e568cede4
thln2ejz <ykxvqz@pm.me>
2024-01-17 11:53:25 +0200

    revised Description, color names

--- otpcrypt.sh
+++ otpcrypt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 : '
-Functions and a demo for encrypting/decrypting via a one-time pad (OTP).
+Utility functions for encrypting/decrypting via a one-time pad (OTP).
 
 USAGE: ./otpcrypt.sh [ -h ]
 
@@ -12,25 +12,25 @@ OUTPUT:
 
 DESCRIPTION:
 
-- Function ascii_to_binary() requires plaintext input (e.g. "I am Bob.")
-and will convert the input into a binary string containing no spaces.
-- Function binary_to_ascii() requires a binary string input (without spaces)
-and converts it to plaintext by parsing and transforming every 8 bits
-into a character.
-- Function generate_otp() requires a binary string input as returned by
+- ascii_to_binary() accepts plaintext input (e.g. "I am Bob.") and
+converts it into a binary string containing no spaces.
+- binary_to_ascii() accepts a binary string input (without spaces) and
+converts it to plaintext by parsing and transforming every 8 bits into a
+character.
+- generate_otp() accepts a binary string input as returned by
 ascii_to_binary() and generates a _random_ binary string of the same
-length, L, by randomly shuffling the sequence of integers [1,...,L] and
-then applying modulo 2 on each, effectively transforming even numbers to 0
-and odd numbers to 1.
-- Function xor() requires two binary string inputs and computes their XOR
+length, L. It works by randomly shuffling the sequence of integers from
+1 to L and then applying modulo 2 on each, effectively transforming even
+numbers to 0 and odd numbers to 1.
+- xor() requires two binary strings as input and computes their XOR
 (bitwise), i.e. if bits are matching, the result is 0, otherwise 1.
-- Function main() runs a demo of the encryption/decryption procedure.
+- main() runs a demo of the encryption/decryption procedure.
 
 EXAMPLE:
 
-An example is demonstrated in main(). Running the script will show the
+An example is demonstrated in main(). Running the script shows the
 output. The plaintext sample used is: "I am Bob.". This is converted into
-a binary string, an OTP is generated of the same length, the two binary
+a binary string, an OTP of the same length is generated, the two binary
 strings are then XORed, and the result is decrypted back into plaintext.
 
 Sample output:
@@ -63,7 +63,7 @@ ascii_to_binary() {
 }
 
 binary_to_ascii() {
-    string=$(echo "$1" | sed 's/.\{8\}/&\ /g')
+    string=$(echo "${1}" | sed 's/.\{8\}/&\ /g')
     for i in $string; do
         echo "obase=16; ibase=2; $i" |
         bc                           |
@@ -73,29 +73,28 @@ binary_to_ascii() {
 }
 
 xor() {
-    string1=$(echo "$1" | tr -d ' ')
-    string2=$(echo "$2" | tr -d ' ')
+    string1=$(echo "${1}" | tr -d ' ')
+    string2=$(echo "${2}" | tr -d ' ')
 
     if [ "${#string1}" -ne "${#string2}" ]; then
         echo 'Strings are not of equal length!'
         return 1
     else
-        for i in `seq 1 ${#string1}`; do
+        for i in $(seq 1 ${#string1}); do
             [ "${string1:$((i-1)):1}" == "${string2:$((i-1)):1}" ] && echo -n '0' || echo -n '1'
         done
     fi
 }
 
 generate_otp() {
-    local string="$1"
+    local string="${1}"
     local length="${#string}"
-    for i in `shuf -i 1-"$length"`; do
+    for i in `shuf -i 1-"${length}"`; do
         echo -n $((i % 2))
     done
 }
 
 main() {
-    # Parse
     while getopts 'h' option; do
         case "$option" in
             h) print_usage; exit 0         ;;
@@ -104,30 +103,32 @@ main() {
         esac
     done
 
-    # Demo
+    # demo
     plaintext_ascii='I am Bob.'
-    plaintext_binary="$(ascii_to_binary "$plaintext_ascii")"
-    key="$(generate_otp "$plaintext_binary")"
-    ciphertext_binary="$(xor "$plaintext_binary" "$key")"
-    ciphertext_ascii="$(binary_to_ascii "$ciphertext_binary")"
-    decrypted_binary="$(xor "$ciphertext_binary" "$key")"
-    decrypted_ascii="$(binary_to_ascii "$decrypted_binary")"
+    plaintext_binary="$(ascii_to_binary "${plaintext_ascii}")"
+    key="$(generate_otp "${plaintext_binary}")"
+    ciphertext_binary="$(xor "${plaintext_binary}" "${key}")"
+    ciphertext_ascii="$(binary_to_ascii "${ciphertext_binary}")"
+    decrypted_binary="$(xor "${ciphertext_binary}" "${key}")"
+    decrypted_ascii="$(binary_to_ascii "${decrypted_binary}")"
 
-    grn='\e[32m'
-    blu='\e[34m'
-    rst='\e[0m'
+    GREEN='\e[32m'
+    BLUE='\e[34m'
+    DEFAULT='\e[0m'
 
     {
 
-    printf "${blu}Plaintext (ASCII)     : ${grn}%s${rst}\n" "$plaintext_ascii  "
-    printf "${blu}Plaintext (binary)    : ${grn}%s${rst}\n" "$plaintext_binary "
-    printf "${blu}OTP/Key               : ${grn}%s${rst}\n" "$key              "
-    printf "${blu}Ciphertext (binary)   : ${grn}%s${rst}\n" "$ciphertext_binary"
-    printf "${blu}Decrypted msg (binary): ${grn}%s${rst}\n" "$decrypted_binary "
-    printf "${blu}Decrypted msg (ASCII) : ${grn}%s${rst}\n" "$decrypted_ascii  "
+    printf "${BLUE}Plaintext (ASCII)     : ${GREEN}%s${DEFAULT}\n" "${plaintext_ascii}  "
+    printf "${BLUE}Plaintext (binary)    : ${GREEN}%s${DEFAULT}\n" "${plaintext_binary} "
+    printf "${BLUE}OTP/Key               : ${GREEN}%s${DEFAULT}\n" "${key}              "
+    printf "${BLUE}Ciphertext (binary)   : ${GREEN}%s${DEFAULT}\n" "${ciphertext_binary}"
+    printf "${BLUE}Decrypted msg (binary): ${GREEN}%s${DEFAULT}\n" "${decrypted_binary} "
+    printf "${BLUE}Decrypted msg (ASCII) : ${GREEN}%s${DEFAULT}\n" "${decrypted_ascii}  "
 
     } | column -t -s ':'
 
 }
 
-main "$@"
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "${@}"
+fi
