2e96e318f71b1a3c98762fee7e446becaaf55e12
ykxvqze <ykxvqz@pm.me>
2025-07-08 12:45:20 +0300

    add no issues msg

--- troubleshoot.sh
+++ troubleshoot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 << 'EOF'
 Script to help troubleshoot CPU, Memory, and I/O usage issues.
 EOF
@@ -6,35 +7,45 @@ EOF
 << 'EOF'
 The script was tested using the following packages:
 
-package  : procps
-version  : 2:3.3.15-2
-includes : free, uptime, top, vmstat
+Package  : procps
+Version  : 2:3.3.15-2
+Includes : free, uptime, top, vmstat
 
-package  : sysstat
-version  : 12.0.3-2
-includes : mpstat, iostat, pidstat, sar
+Package  : sysstat
+Version  : 12.0.3-2
+Includes : mpstat, iostat, pidstat, sar
 EOF
 
 #
 # functions
 #
 
-__ts__check_sysinfo () { :; }
-__ts__load_colors   () { :; }
-__ts__check_free    () { :; }
-__ts__check_uptime  () { :; }
-__ts__check_top     () { :; }
-__ts__check_vmstat  () { :; }
-__ts__check_mpstat  () { :; }
-__ts__check_iostat  () { :; }
-__ts__check_sar     () { :; }
-__ts__check_pidstat () { :; }
+__load_color    () { :; }
+__check_sysinfo () { :; }
+__check_free    () { :; }
+__check_uptime  () { :; }
+__check_top     () { :; }
+__check_vmstat  () { :; }
+__check_mpstat  () { :; }
+__check_iostat  () { :; }
+__check_sar     () { :; }
+__check_pidstat () { :; }
+
+#
+# colors
+#
+
+__load_color() {
+    export RED="$(tput setaf 1)"
+    export GREEN="$(tput setaf 2)"
+    export DEFAULT="$(tput sgr0)"
+}
 
 #
 # system info
 #
 
-__ts__check_sysinfo() {
+__check_sysinfo() {
     superuser=$(grep ':x:0:' /etc/passwd | cut -d ':' -f 1)
 
     ip_public=$(wget -q -O - 'ipinfo.io/ip')
@@ -75,19 +86,10 @@ __ts__check_sysinfo() {
 }
 
 #
-# colors
-#
-
-__ts__load_colors() {
-    RED="$(tput setaf 1)"
-    DEFAULT="$(tput sgr0)"
-}
-
-#
 # free -m
 #
 
-__ts__check_free() {
+__check_free() {
     echo
     echo "[*] Checking free -m..."
 
@@ -98,11 +100,9 @@ __ts__check_free() {
         return 1
     fi
 
-    __ts__load_colors
-
     # thresholds
     local memory_threshold=70                # 70%
-    local available_memory_threshold=2048    # 2 GB in MB
+    local available_memory_threshold=2048    # 2 GB
     local swap_threshold=10                  # 10%
 
     # metrics
@@ -116,23 +116,33 @@ __ts__check_free() {
     memory_usage_percent=$(( 100 * used_memory / total_memory ))
     swap_usage_percent=$(( 100 * used_swap / total_swap ))
 
+    # flag
+	issues=0
+
     # check memory usage
     if [ "${memory_usage_percent}" -ge "${memory_threshold}" ]; then
-        echo -e "[ ${RED}Warning${DEFAULT} ] Memory usage is high: ${memory_usage_percent}% used."
+        echo "[ ${RED}Warning${DEFAULT} ] Memory usage is high: ${memory_usage_percent}% used."
+        issues=1
     fi
 
     # check available memory
     if [ "${available_memory}" -le "${available_memory_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] Available memory is low: ${available_memory} MB."
+        issues=1
     fi
 
     # check swap usage
     if [ "${total_swap}" -gt 0 ]; then
         if [ "${swap_usage_percent}" -ge "${swap_threshold}" ]; then
             echo "[ ${RED}Warning${DEFAULT} ] Swap usage is high: ${swap_usage_percent}% used."
+            issues=1
         fi
     else
         echo "No swap space configured."
+    fi
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
     fi
 }
 
@@ -140,7 +150,7 @@ __ts__check_free() {
 # uptime (load average)
 #
 
-__ts__check_uptime() {
+__check_uptime() {
     echo
     echo "[*] Checking load average (uptime)..."
 
@@ -151,16 +161,22 @@ __ts__check_uptime() {
         return 1
     fi
 
-    __ts__load_colors
-
     # number of physical cores
     num_cores=$(grep "core id" /proc/cpuinfo | sort -u | wc -l)
 
     # 1-minute load average
     load_average=$(uptime | tr ',' ' ' | awk '{print $((NF-2))}')
 
+    # flag
+	issues=0
+
     if (( $(echo "${load_average} >= ${num_cores}" | bc -l) )); then
         echo "[ ${RED}Warning${DEFAULT} ] Load average (${load_average}) is greater than or equal to the number of physical CPUs (${num_cores})."
+        issues=1
+    fi
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
     fi
 }
 
@@ -168,7 +184,7 @@ __ts__check_uptime() {
 # top
 #
 
-__ts__check_top() {
+__check_top() {
     echo
     echo "[*] Checking top..."
 
@@ -178,8 +194,6 @@ __ts__check_top() {
         echo "Then rerun the script. Exiting..."
         return 1
     fi
-
-    __ts__load_colors
 
     # number of physical cores
     num_cores=$(grep "core id" /proc/cpuinfo | sort -u | wc -l)
@@ -192,7 +206,7 @@ __ts__check_top() {
     local cpu_process_threshold=$((70 * num_cores))  # CPU usage for specific processes (%)
     local memory_process_threshold=70                # memory usage for specific processes (%)
 
-    # cpu metrics from top
+    # cpu metrics from `top`
     top_output="$(top -b -n 1)"
     cpu_metrics="$(echo "${top_output}" | grep "Cpu(s)" | tr ',' ' ')"
     cpu_user="$(echo "${cpu_metrics}"   | awk '{print $2}')"
@@ -200,37 +214,58 @@ __ts__check_top() {
     cpu_idle="$(echo "${cpu_metrics}"   | awk '{print $8}')"
     cpu_iowait="$(echo "${cpu_metrics}" | awk '{print $10}')"
 
+    # flag
+	issues=0
+
     # check CPU usage thresholds
     if (( $(echo "${cpu_user} >= ${cpu_user_threshold}" | bc -l) )); then
         echo "[ ${RED}Warning${DEFAULT} ] User CPU usage is high: ${cpu_user}%."
+        issues=1
     fi
 
     if (( $(echo "${cpu_system} >= ${cpu_system_threshold}" | bc -l) )); then
         echo "[ ${RED}Warning${DEFAULT} ] System CPU usage is high: ${cpu_system}%."
+        issues=1
     fi
 
     if (( $(echo "${cpu_idle} <= ${cpu_idle_threshold}" | bc -l) )); then
         echo "[ ${RED}Warning${DEFAULT} ] Idle CPU usage is low: ${cpu_idle}%."
+        issues=1
     fi
 
     if (( $(echo "${cpu_iowait} >= ${cpu_iowait_threshold}" | bc -l) )); then
         echo "[ ${RED}Warning${DEFAULT} ] I/O wait is high: ${cpu_iowait}%."
+        issues=1
     fi
 
-    # check specific processes for high CPU and memory usage
-    echo "${top_output}" | awk -v high_cpu="${cpu_process_threshold}" -v high_mem="${memory_process_threshold}" '
-    NR>7 {
-        if ($9 >= high_cpu)  { printf "[ Warning ] Process %s is using high CPU: %s%%\n", $12, $9 }
-        if ($10 >= high_mem) { printf "[ Warning ] Process %s is using high memory: %s%%\n", $12, $10}
-    }
-    '
+    # check specific processes for high CPU and memory usage 
+    while read -r line; do
+        process="$(echo "$line" | awk '{print $12}')"
+        cpu="$(echo "$line" | awk '{print $9}')"
+        memory="$(echo "$line" | awk '{print $10}')"
+        
+        if (( $(echo "${cpu} >= ${cpu_process_threshold}" | bc -l) )); then
+            echo "[ ${RED}Warning${DEFAULT} ] Process $process is using high CPU: $cpu%"
+            issues=1
+        fi
+        
+        if (( $(echo "${memory} >= ${memory_process_threshold}" | bc -l) )); then
+            echo "[ ${RED}Warning${DEFAULT} ] Process $process is using high memory: $memory%"
+            issues=1
+        fi
+
+    done <<< $(echo "${top_output}" | awk 'NR >7')
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
+    fi
 }
 
 #
 # vmstat
 #
 
-__ts__check_vmstat() {
+__check_vmstat() {
     echo
     echo "[*] Checking vmstat..."
 
@@ -240,8 +275,6 @@ __ts__check_vmstat() {
         echo "Then rerun the script. Exiting..."
         return 1
     fi
-
-    __ts__load_colors
 
     # number of physical cores
     local num_cores=$(grep "core id" /proc/cpuinfo | sort -u | wc -l)
@@ -271,41 +304,57 @@ __ts__check_vmstat() {
     id=$(echo "${vmstat_output}" | awk '{print $15}')          # idle CPU time
     wa=$(echo "${vmstat_output}" | awk '{print $16}')          # I/O wait
 
+    # flag
+    issues=0
+
     # check thresholds
     if [ "${r}" -ge "${num_cores}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] Run queue is high: ${r} processes waiting for CPU."
+        issues=1
     fi
 
     if [ "${free_memory}" -lt "${free_memory_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] Free memory is low: ${free_memory} MB."
+        issues=1
     fi
 
     if [ "${si}" -gt "${swap_in_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] Swap in is occurring: ${si} MB."
+        issues=1
     fi
 
     if [ "${so}" -gt "${swap_out_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] Swap out is occurring: ${so} MB."
+        issues=1
     fi
 
     if [ "${bi}" -gt "${io_threshold}" ] || [ "${bo}" -gt "${io_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] High I/O activity: ${bi} blocks in, ${bo} blocks out."
+        issues=1
     fi
 
     if [ "${us}" -ge "${cpu_user_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] User CPU usage is high: ${us}%."
+        issues=1
     fi
 
     if [ "${sy}" -ge "${cpu_system_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] System CPU usage is high: ${sy}%."
+        issues=1
     fi
 
     if [ "${id}" -lt "${cpu_idle_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] Idle CPU time is low: ${id}%. The system may be overloaded."
+        issues=1
     fi
 
     if [ "${wa}" -gt "${cpu_iowait_threshold}" ]; then
         echo "[ ${RED}Warning${DEFAULT} ] I/O wait is high: ${wa}%. Processes are waiting on I/O."
+        issues=1
+    fi
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
     fi
 }
 
@@ -313,7 +362,7 @@ __ts__check_vmstat() {
 # mpstat -P ALL
 #
 
-__ts__check_mpstat() {
+__check_mpstat() {
     echo
     echo "[*] Checking mpstat..."
 
@@ -323,8 +372,6 @@ __ts__check_mpstat() {
         echo "Then rerun the script. Exiting..."
         return 1
     fi
-
-    __ts__load_colors
 
     # threshold values
     local cpu_user_threshold=70      # user CPU usage threshold (%)
@@ -336,6 +383,9 @@ __ts__check_mpstat() {
 
     # mpstat output for all CPUs
     mpstat_output="$(mpstat -P ALL 1 1 | grep 'Average' | sed -n '3,$ p')"
+
+    # flag
+    issues=0
 
     # parse and check each line for CPU usage
     while read -r line; do
@@ -350,36 +400,46 @@ __ts__check_mpstat() {
         # check thresholds
         if (( $(echo "${usr} >= ${cpu_user_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] CPU ${cpu_id} - User CPU usage is high: ${usr}%."
+            issues=1
         fi
 
         if (( $(echo "${sys} >= ${cpu_system_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] CPU ${cpu_id} - System CPU usage is high: ${sys}%."
+            issues=1
         fi
 
         if (( $(echo "${idle} <= ${cpu_idle_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] CPU ${cpu_id} - Idle CPU time is low: ${idle}%. The system may be overloaded."
+            issues=1
         fi
 
         if (( $(echo "${iowait} >= ${cpu_iowait_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] CPU ${cpu_id} - I/O wait is high: ${iowait}%. Processes are waiting on I/O."
+            issues=1
         fi
 
         if (( $(echo "${irq} >= ${cpu_irq_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] CPU ${cpu_id} - IRQ time is high: ${irq}%. This may indicate hardware issues."
+            issues=1
         fi
 
         if (( $(echo "${softirq} >= ${cpu_softirq_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] CPU ${cpu_id} - SoftIRQ time is high: ${softirq}%. This may indicate network or software delays."
+            issues=1
         fi
 
     done <<< "${mpstat_output}"
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
+    fi
 }
 
 #
 # iostat -x 1
 #
 
-__ts__check_iostat() {
+__check_iostat() {
     echo
     echo "[*] Checking iostat..."
 
@@ -389,8 +449,6 @@ __ts__check_iostat() {
         echo "Then rerun the script. Exiting..."
         return 1
     fi
-
-    __ts__load_colors
 
     # threshold values
     local utilization_threshold=70      # utilization threshold (%)
@@ -402,6 +460,9 @@ __ts__check_iostat() {
     # iostat output - ignore the first result (i.e., stats since boot time) 
     n_lines_iostat="$(iostat -x 1 1 | wc -l)"
     iostat_output="$(iostat -x 1 2 | sed -n "$((n_lines_iostat +1)),$ p" | sed -n '/Device/,$ p' | tail -n +2)"
+
+    # flag
+    issues=0
 
     # parse and check each line for disk I/O performance
     while read -r line; do
@@ -415,32 +476,41 @@ __ts__check_iostat() {
         # check thresholds
         if (( $(echo "${utilization} >= ${utilization_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] Device ${device} - Utilization is high: ${utilization}%."
+            issues=1
         fi
 
         if (( $(echo "${read_await} >= ${read_wait_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] Device ${device} - Average read wait time is high: ${read_await} ms."
+            issues=1
         fi
 
         if (( $(echo "${write_await} >= ${write_wait_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] Device ${device} - Average write wait time is high: ${write_await} ms."
+            issues=1
         fi
 
         if (( $(echo "${queue_size} >= ${queue_size_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] Device ${device} - Average queue size is high: ${queue_size}."
+            issues=1
         fi
 
         if (( $(echo "${service_time} >= ${service_time_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] Device ${device} - Service time is high: ${service_time} ms."
+            issues=1
         fi
 
     done <<< "${iostat_output}"
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
+    fi
 }
 
 #
 # sar -n DEV 1
 #
 
-__ts__check_sar() {
+__check_sar() {
     echo
     echo "[*] Checking sar..."
 
@@ -451,8 +521,6 @@ __ts__check_sar() {
         return 1
     fi
 
-    __ts__load_colors
-
     # threshold values
     # local rx_packets_threshold=1000  # receive packets per second
     # local tx_packets_threshold=1000  # transmit packets per second
@@ -462,6 +530,9 @@ __ts__check_sar() {
 
     # sar output for network devices
     sar_output="$(sar -n DEV 1 1 | grep 'Average' | sed -n '2,$ p')"
+
+    # flag
+    issues=0
 
     # parse and check each line for network performance
     while read -r line; do
@@ -492,16 +563,21 @@ __ts__check_sar() {
 EOF
         if (( $(echo "${ifutil} >= ${ifutil_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] Device ${device} - Interface utilization is high: ${ifutil}%."
+            issues=1
         fi
 
     done <<< "${sar_output}"
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
+    fi
 }
 
 #
 # pidstat 1
 #
 
-__ts__check_pidstat() {
+__check_pidstat() {
     echo
     echo "[*] Checking pidstat..."
 
@@ -511,8 +587,6 @@ __ts__check_pidstat() {
         echo "Then rerun the script. Exiting..."
         return 1
     fi
-
-    __ts__load_colors
 
     # number of physical cores
     local num_cores=$(grep "core id" /proc/cpuinfo | sort -u | wc -l)
@@ -526,6 +600,9 @@ __ts__check_pidstat() {
     # pidstat output
     pidstat_output="$(pidstat | sed -n '4,$ p')"
 
+    # flag
+    issues=0
+
     # parse and check each line for process performance
     while read -r line; do
         pid=$(echo "${line}"        | awk '{print $4}')
@@ -537,33 +614,42 @@ __ts__check_pidstat() {
         # check thresholds
         if (( $(echo "${user_cpu} >= ${cpu_user_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] PID ${pid} - User CPU time is high: ${user_cpu}%."
+            issues=1
         fi
 
         if (( $(echo "${system_cpu} >= ${cpu_system_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] PID ${pid} - System CPU time is high: ${system_cpu}%."
+            issues=1
         fi
 
         if (( $(echo "${iowait} >= ${cpu_iowait_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] PID ${pid} - I/O wait is high: ${iowait}%. This may indicate slow I/O operations."
+            issues=1
         fi
 
         if (( $(echo "${cpu_usage} > ${cpu_usage_threshold}" | bc -l) )); then
             echo "[ ${RED}Warning${DEFAULT} ] PID ${pid} - %CPU usage is high: ${cpu_usage}."
+            issues=1
         fi
 
     done <<< "${pidstat_output}"
+
+    if [ "$issues" -eq 0 ]; then
+        echo "${GREEN}No issues found.${DEFAULT}"
+    fi
 }
 
 main() {
-    __ts__check_sysinfo
-    __ts__check_free
-    __ts__check_uptime
-    __ts__check_top
-    __ts__check_vmstat
-    __ts__check_mpstat
-    __ts__check_iostat
-    __ts__check_sar
-    __ts__check_pidstat
+	__load_color
+    __check_sysinfo
+    __check_free
+    __check_uptime
+    __check_top
+    __check_vmstat
+    __check_mpstat
+    __check_iostat
+    __check_sar
+    __check_pidstat
 }
 
 if [ "${BASH_SOURCE[0]}" == "${0}" ]; then
