2b950b0d22c44281428d5177cd0cd9283e7899e5
ykxvqze <ykxvqz@pm.me>
2025-07-08 16:20:46 +0300

    refactor sshban.sh

--- sshban.sh
+++ sshban.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-: '
+
+<< 'EOF'
 Process server authentication logs, send alert and ban repeated failed SSH attempts.
 
 USAGE: ./sshban.sh [ -h ]
@@ -27,47 +28,77 @@ address appears more than 4 times _and_ is not already logged in
 
 Note: execute the script as a cron job (e.g. every 2 minutes).
 */2 * * * * /path/to/sshban.sh
-
-'
+EOF
 
 ssh_blacklist='ssh_blacklist.txt'
 ssh_badlogin='/tmp/ssh_badlogin.txt'
 logfile='/tmp/logfile.txt'
 recipient='sysadmin'
 
-print_usage() {
-    echo -e "sshban.sh:
+__check_euid        () { :; }
+__print_usage       () { :; }
+__parse_options     () { :; }
+__ban_ip            () { :; }
+__add_to_blacklist  () { :; }
+__notify_mail       () { :; }
+__is_ip_blacklisted () { :; }
+__get_log           () { :; }
+__process_log       () { :; }
+__main              () { :; }
+
+__check_euid() {
+    if [ "$EUID" != 0 ]; then
+        echo "Use sudo to run the script: sudo ./${0##*/}"
+        echo "Exiting..."
+        exit 1
+    fi
+}
+
+__print_usage() {
+    echo -e "
     Usage:
-    ./${0##*/}             Check /var/log/auth.log, send alert and ban repeated failed SSH attempts
-    ./${0##*/} [ -h ]      Print usage and exit
-    ./${0##*/} [ -l ]      List the log\n"
+
+    ./${0##*/} [ -h ]           Print usage and exit
+    sudo ./${0##*/}             Check /var/log/auth.log, send alert and ban repeated failed SSH attempts
+    sudo ./${0##*/} [ -l ]      List the log\n"
 }
 
-ban_ip() {
+__parse_options() {
+    while getopts 'hl' option; do
+        case $option in
+            h) __print_usage;  exit 0 ;;
+            l) __check_euid; __get_log;;
+            *) echo -e 'Incorrect usage!\n';
+               __print_usage;  exit 1 ;;
+        esac
+    done
+}
+
+__ban_ip() {
     IP="${1}"
-    sudo iptables -A INPUT -s "${IP}" -j DROP
-    sudo iptables-save
+    iptables -A INPUT -s "${IP}" -j DROP
+    iptables-save
 }
 
-add_to_blacklist() {
+__add_to_blacklist() {
     IP="${1}"
     echo "${IP}" >> "${ssh_blacklist}"
 }
 
-notify_mail() {
+__notify_mail() {
     IP="${1}"
     n_attempts="${2}"
     echo "[!] Notification of failed SSH login attempts from ${IP} (${n_attempts} failed attempts). IP address has been blocked." |
     mail -s 'SSH failed logins' "${recipient}"
 }
 
-is_ip_blacklisted() {
+__is_ip_blacklisted() {
     IP="${1}"
     grep "${IP}" "${ssh_blacklist}" &> /dev/null && return 0 || return 1
 }
 
-get_log() {
-    sudo cat /var/log/auth.log |
+__get_log() {
+    cat /var/log/auth.log      |
     grep -i 'sshd.*fail'       |
     grep -ho 'rhost=.*\s'      |
     grep -Eo '[^=]+$' > "${ssh_badlogin}"
@@ -75,23 +106,13 @@ get_log() {
     sort "${ssh_badlogin}" |
     uniq -c                |
     sort -nr -k 1          |
-    sed -E 's/\s //g'
+    sed -E 's/\s //g' > "${logfile}"
 }
 
-main() {
-    while getopts 'hl' option; do
-        case $option in
-            h) print_usage;  exit 0 ;;
-            l) get_log              ;;
-            *) echo -e 'Incorrect usage!\n';
-               print_usage;  exit 1 ;;
-        esac
-    done
-
+__process_log() {
     [ -f "${ssh_badlogin}" ] && rm -rf "${ssh_badlogin}"
     [ ! -f "${ssh_blacklist}" ] && touch "${ssh_blacklist}"
 
-    get_log > "${logfile}"
     n_entries=$(cat ${logfile} | wc -l)
     n_attempts=($(cut -d ' ' -f 1 "${logfile}"))
     ip_list=($(cut -d ' ' -f 2 "${logfile}"))
@@ -101,13 +122,22 @@ main() {
         IP="${ip_list[$index]}"
         n="${n_attempts[$index]}"
         if [ "$n" -gt 4 ]; then
-            if [ ! $(is_ip_blacklisted "${IP}") ]; then
-                add_to_blacklist "${IP}"
-                ban_ip "${IP}"
-                notify_mail "${IP}" "${n}"
+            if [ ! $(__is_ip_blacklisted "${IP}") ]; then
+                __add_to_blacklist "${IP}"
+                __ban_ip "${IP}"
+                __notify_mail "${IP}" "${n}"
             fi
         fi
     done
 }
 
-main "$@"
+__main() {
+    __parse_options "$@"
+    __check_euid
+    __get_log
+    __process_log
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    __main "$@"
+fi
