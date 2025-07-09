72ea9afb2671130310d6ac34743d39acce1ec0ad
ykxvqze <ykxvqz@pm.me>
2025-07-09 09:54:18 +0300

    refactor functions

--- clr.sh
+++ clr.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-: '
+
+<< 'EOF'
 Clear Firefox data, bookmarks, cookies, cache, and other files
 
 USAGE:  ./clr.sh [ -h ]
@@ -18,54 +19,76 @@ and other files within target directories. Patterns sought include:
 `cookies`, `places`, `history`, `webappsstore`, and `bookmarks`,
 `datareporting`, `bookmarkbackups`. System thumbnail cache located at
 ~/.cache/thumbnails is also cleared.
-'
+EOF
 
-trap 'echo error on line: $LINENO' ERR
+__set_trap() {
+    trap 'echo error on line: $LINENO' ERR
+}
 
-print_usage() {
-    echo -e "clr.sh: clear Firefox cache, cookies, and other files.
+__print_usage() {
+    echo -e "Clear Firefox cache, cookies, and other files.
+
     Usage:
+
           ./${0##*/}
           ./${0##*/} [ -h ]    Print usage and exit\n"
 }
 
-while getopts 'h' option; do
-    case $option in
-        h) print_usage;  exit 0 ;;
-        *) echo -e 'Incorrect usage. See below:\n';
-           print_usage;  exit 1 ;;
-    esac
-done
+__parse_options() {
+    while getopts 'h' option; do
+        case "$option" in
+            h) __print_usage;  exit 0 ;;
+            *) echo -e 'Incorrect usage. See below:\n';
+               __print_usage;  exit 1 ;;
+        esac
+    done
+}
 
-# Firefox
-cd ~/.cache/mozilla/firefox/
-find . -type f ! -path "./*/settings/*"     \
-               ! -path "./*/startupCache/*" \
-               ! -path "./*/safebrowsing/*" \
-                 -exec bash -c '>"{}"'      \;
+__clear_cache() {
+    #
+    # Firefox
+    #
+    cd ~/.cache/mozilla/firefox/
+    find . -type f ! -path "./*/settings/*"     \
+                   ! -path "./*/startupCache/*" \
+                   ! -path "./*/safebrowsing/*" \
+                     -exec bash -c '>"{}"'      \;
 
-cd ~/.mozilla/firefox/
-find . -type f -name "cookies*"      \
-            -o -name "places*"       \
-            -o -name "*history*"     \
-            -o -name "webappsstore*" \
-            -o -name "bookmarks*"    \
-               -exec bash -c '>"{}"' \;
+    cd ~/.mozilla/firefox/
+    find . -type f -name "cookies*"      \
+                -o -name "places*"       \
+                -o -name "*history*"     \
+                -o -name "webappsstore*" \
+                -o -name "bookmarks*"    \
+                   -exec bash -c '>"{}"' \;
 
-find . -type f -path "./*/datareporting/*"    \
-            -o -path "./*/bookmarkbackups/*"  \
-               -exec bash -c '>"{}"; rm "{}"' \;
+    find . -type f -path "./*/datareporting/*"    \
+                -o -path "./*/bookmarkbackups/*"  \
+                   -exec bash -c '>"{}"; rm "{}"' \;
 
-# other
-cd ~/.cache/thumbnails
-find . -type f -exec bash -c '>"{}"; rm "{}"' \;
+    #
+    # Other
+    #
+    cd ~/.cache/thumbnails
+    find . -type f -exec bash -c '>"{}"; rm "{}"' \;
 
 << 'EOF'
-cd ~/.cache/thunderbird/
-find . -type f -exec bash -c '>"{}"; rm "{}"' \;
+    cd ~/.cache/thunderbird/
+    find . -type f -exec bash -c '>"{}"; rm "{}"' \;
 
-# custom temp folder
-cd ~/Documents/temp
-find . -type f -exec bash -c '>"{}"; rm "{}"' \;
-rm -rf ~/Documents/temp
+    # custom temp folder
+    cd ~/Documents/temp
+    find . -type f -exec bash -c '>"{}"; rm "{}"' \;
+    rm -rf ~/Documents/temp
 EOF
+}
+
+__main() {
+    __set_trap
+    __parse_options "$@"
+    __clear_cache
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    __main "$@"
+fi
