7017ea3d5e12cee1f9b9d84ab0200ce5275e962d
ykxvqze <ykxvqz@pm.me>
2025-09-15 19:52:30 +0300

    refactor

--- iptx.sh
+++ iptx.sh
@@ -2,78 +2,64 @@
 << 'EOF'
 Utility functions for IPv4 networking/transformations
 
-USAGE: . ./iptx.sh
-       __cidr2network 128.42.5.4/21
-       __cidr2broadcast 128.42.5.4/21
-
-       ./iptx.sh 128.42.5.4/21
+USAGE:
+        ./iptx.sh <CIDR_address>
 
 OPTIONS:
        [ -h ]              Print usage
 
 EXAMPLES:
-         . ./iptx.sh                     # source the script
-         __cidr2network 128.42.5.4/21    # get network address
-         __cidr2netmask 128.42.5.4/21    # get netmask
-         __cidr2broadcast 128.42.5.4/21  # get broadcast address
-         __cidr2ipfirst 128.42.5.4/21    # get first usable IP address
-         __cidr2iplast 128.42.5.4/21     # get last usable IP address
-         __cidr2numhosts 128.42.5.4/21   # get number of usable hosts
-
-        ./iptx.sh 128.42.5.4/21          # get printout summary of all info
+        ./iptx.sh 128.42.5.4/21          # get printout summary
 
 DESCRIPTION:
 
-* __valid_ipv4() - check if an IPv4 address is valid or not.
-* __valid_cidr() - check if CIDR notation is valid or not.
-* __ip2bin() - convert an IPv4 address from dotted-decimal notation to binary.
-* __bin2ip() - convert an IPv4 address from binary to dotted-decimal notation.
-* __cidr2netmask() - extract the netmask from an IPv4 address given in CIDR notation.
-* __cidr2network() - extract the network address from a CIDR IPv4 address.
-* __cidr2broadcast() - extract the broadcast address from a CIDR IPv4 address.
-* __cidr2numhosts() - extract the number of hosts that can be assigned IP addresses.
-* __cidr2ipfirst() - extract the first usable IP address from CIDR notation.
-* __cidr2iplast() - extract the last usable IP address from CIDR notation.
+* __valid_ipv4()     : check if an IPv4 address is valid or not.
+* __valid_cidr()     : check if CIDR notation is valid or not.
+* __ip2bin()         : convert an IPv4 address from dotted-decimal notation to binary.
+* __bin2ip()         : convert an IPv4 address from binary to dotted-decimal notation.
+* __cidr2netmask()   : extract the netmask from an IPv4 address given in CIDR notation.
+* __cidr2network()   : extract the network address from a CIDR IPv4 address.
+* __cidr2broadcast() : extract the broadcast address from a CIDR IPv4 address.
+* __cidr2numhosts()  : extract the number of hosts that can be assigned IP addresses.
+* __cidr2ipfirst()   : extract the first usable IP address from CIDR notation.
+* __cidr2iplast()    : extract the last usable IP address from CIDR notation.
 
 EXAMPLES:
 
 __ip2bin 128.42.5.4                             # 10000000 00101010 00000101 00000100
 __bin2ip '10000000;00101010;00000101;00000100'  # 128.42.5.4
-__cidr2network 128.42.5.4/21                    # 128.42.0.0
-__cidr2netmask 128.42.5.4/21                    # 255.255.248.0
-__cidr2broadcast 128.42.5.4/21                  # 128.42.7.255
-__cidr2numhosts 128.42.5.4/21                   # 2046
-__cidr2ipfirst 128.42.5.4/21                    # 128.42.0.1
-__cidr2iplast 128.42.5.4/21                     # 128.42.7.254
 
 ./iptx.sh 128.42.5.4/21
 
-Network address          128.42.0.0
-Broadcast address        128.42.7.255
-Subnet mask              255.255.248.0
-First usable address     128.42.0.1
-Last usable address      128.42.7.254
-Number of usable hosts   2046
+Network address        : 128.42.0.0
+Broadcast address      : 128.42.7.255
+Subnet mask            : 255.255.248.0
+First usable address   : 128.42.0.1
+Last usable address    : 128.42.7.254
+Number of usable hosts : 2046
 
 EOF
 
-__print_usage    () { :; }  # print usage
-__valid_ipv4     () { :; }  # Is IPv4 address valid?
-__valid_cidr     () { :; }  # Is CIDR address valid?
-__zero_pad       () { :; }  # zero pad
-__ip2bin         () { :; }  # IPv4 (decimal-dotted) to binary
-__bin2ip         () { :; }  # binary to IPv4 (decimal-dotted)
-__cidr2netmask   () { :; }  # CIDR to netmask address
-__cidr2network   () { :; }  # CIDR to network address
-__cidr2broadcast () { :; }  # CIDR to broadcast address
-__cidr2numhosts  () { :; }  # CIDR to number of hosts
-__cidr2ipfirst   () { :; }  # CIDR to first usable IP address
-__cidr2iplast    () { :; }  # CIDR to last usable IP address
-__parse_options  () { :; }  # parse options
-__parse_argument () { :; }  # parse argument (CIDR address)
-__check_argument () { :; }  # check if argument is valid
-__print_tx       () { :; }  # print transformations
-__main           () { :; }  # main function
+__print_usage        () { :; }  # print usage
+__valid_ipv4         () { :; }  # Is IPv4 address valid?
+__valid_cidr         () { :; }  # Is CIDR address valid?
+__zero_pad           () { :; }  # zero pad
+__ip2bin             () { :; }  # IPv4 (decimal-dotted) to binary
+__bin2ip             () { :; }  # binary to IPv4 (decimal-dotted)
+__get_ip             () { :; }  # get IP address from CIDR
+__get_netmask_length () { :; }  # get netmask length from CIDR
+__get_host_length    () { :; }  # get host length from CIDR
+__get_network_part   () { :; }  # get network part (binary)
+__cidr2netmask       () { :; }  # CIDR to netmask address
+__cidr2network       () { :; }  # CIDR to network address
+__cidr2broadcast     () { :; }  # CIDR to broadcast address
+__cidr2numhosts      () { :; }  # CIDR to number of hosts
+__cidr2ipfirst       () { :; }  # CIDR to first usable IP address
+__cidr2iplast        () { :; }  # CIDR to last usable IP address
+__parse_arguments    () { :; }  # parse arguments
+__validate_arguments () { :; }  # check if arguments are valid
+__print_tx           () { :; }  # print transformations
+__main               () { :; }  # main function
 
 __print_usage() {
     echo -e "iptx.sh: utility for CIDR to IPv4 transformations.
@@ -83,12 +69,13 @@ __print_usage() {
 
            . ./${0##*/}
            __cidr2network 128.42.5.4/21
-           __cidr2broadcast 128.42.5.4/21"
+           __cidr2broadcast 128.42.5.4/21 \n"
 }
 
 __valid_ipv4() {
     local ip="${1}"
     [[ "${ip}" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]
+
     if [ "$?" -ne 0 ]; then
         echo "Invalid Ipv4 address. Exiting..."
         exit 1
@@ -97,6 +84,7 @@ __valid_ipv4() {
 
 __valid_cidr() {
     local ip_cidr="${1}"
+
     if [[ "${ip_cidr}" =~ ^[^/]*/([0-9]|[1-2][0-9]|3[0-2])$ ]]; then
         ip=$(echo "${ip_cidr}" | cut -d '/' -f 1)
         __valid_ipv4 "${ip}"
@@ -106,20 +94,20 @@ __valid_cidr() {
     fi
 }
 
-# example: 110 --> 00000110
+# example: 110 > 00000110
 __zero_pad() {
     local input="${1}"
     n="$(expr length "${input}")"
     z=""
 
-    if [ "${n}" -lt 8 ]; then
+    if [ "$n" -lt 8 ]; then
         d=$((8-n))
         z="$(head -c "$d" /dev/zero | tr '\0' '0')"
     fi
     echo "${z}${input}"
 }
 
-# example: 128.42.5.4 --> 10000000 00101010 00000101 00000100
+# example: 128.42.5.4 > 10000000 00101010 00000101 00000100
 __ip2bin() {
     local ip="${1}"
     x="$(echo "${ip}" | tr '.' ';')"
@@ -127,140 +115,136 @@ __ip2bin() {
     for i in $r; do __zero_pad "${i}"; done | xargs
 }
 
-# example: '10000000;00101010;00000101;00000100' --> 128.42.5.4
+# example: '10000000;00101010;00000101;00000100' > 128.42.5.4
 __bin2ip() {
     local binary_sequence="${1}"
     echo "obase=10; ibase=2; ${binary_sequence}" | bc | xargs | tr ' ' '.'
 }
 
-# example: 128.42.5.4/21 --> 255.255.248.0
+__get_ip() {
+    ip="$(echo "${ip_cidr}" | awk -F '/' '{print $1}')"
+}
+
+__get_netmask_length() {
+    netmask_length="$(echo "${ip_cidr}" | awk -F '/' '{print $2}')"
+}
+
+__get_host_length() {
+    host_length="$((32 - netmask_length))"
+}
+
+__get_network_part() {
+    ip_bin="$(__ip2bin "$ip" | tr -d ' ')"
+    network_part="$(echo "${ip_bin:0:netmask_length}")"
+}
+
+# example: 128.42.5.4/21 > 255.255.248.0
 __cidr2netmask() {
-    local ip_cidr="${1}"
-    __valid_cidr "${ip_cidr}"
-    netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
-    host_length="$((32-netmask_length))"
     ones="$(head -c "${netmask_length}" /dev/zero | tr '\0' '1')"
     zeros="$(head -c "${host_length}" /dev/zero | tr '\0' '0')"
     sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${ones}${zeros}")"
-    __bin2ip "${sequence}"
+    netmask="$(__bin2ip "${sequence}")"
 }
 
-# example: 128.42.5.4/21 --> 128.42.0.0
+# example: 128.42.5.4/21 > 128.42.0.0
 __cidr2network() {
-    local ip_cidr="${1}"
-    __valid_cidr "${ip_cidr}"
-    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
-    local ip="$(echo "${ip_cidr}" | cut -d '/' -f 1)"
-    local ip_bin="$(__ip2bin "$ip" | tr -d ' ')"
-
-    network_part="$(echo "${ip_bin:0:netmask_length}")"
-    host_length="$((32-netmask_length))"
     zeros="$(head -c "${host_length}" /dev/zero | tr '\0' '0')"
     sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${zeros}")"
-    __bin2ip "${sequence}"
+    network="$(__bin2ip "${sequence}")"
 }
 
-# example: 128.42.5.4/21 --> 128.42.7.255
+# example: 128.42.5.4/21 > 128.42.7.255
 __cidr2broadcast() {
-    local ip_cidr="${1}"
-    __valid_cidr "${ip_cidr}"
-    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
-    local ip="$(echo ${ip_cidr} | cut -d '/' -f 1)"
-    local ip_bin="$(__ip2bin "${ip}" | tr -d ' ')"
-
-    network_part="$(echo "${ip_bin:0:netmask_length}")"
-    host_length="$((32-netmask_length))"
     ones="$(head -c "${host_length}" /dev/zero | tr '\0' '1')"
     sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${ones}")"
-    __bin2ip "${sequence}"
+    broadcast="$(__bin2ip "${sequence}")"
 }
 
-# example: 128.42.5.4/21 --> 2046 (number of usable hosts)
+# example: 128.42.5.4/21 > 2046 (number of usable hosts)
 __cidr2numhosts() {
-    local ip_cidr="${1}"
-    __valid_cidr "${ip_cidr}"
-    netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
-    host_length="$((32-netmask_length))"
-    if [ "${host_length}" -eq 0 ]; then
-        echo '0'
+
+    if [[ "${host_length}" -eq 0 ]]; then
+        numhosts="0"
     else
-        echo "$((2**host_length-2))"
+        numhosts="$((2**host_length - 2))"
     fi
 }
 
-# example: 128.42.5.4/21 --> 128.42.0.1 (first usable IP address)
+# example: 128.42.5.4/21 > 128.42.0.1 (first usable IP address)
 __cidr2ipfirst() {
-    local ip_cidr="${1}"
-    __valid_cidr "${ip_cidr}"
-    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
-    local ip="$(echo "${ip_cidr}" | cut -d '/' -f 1)"
-    local ip_bin="$(__ip2bin "${ip}" | tr -d ' ')"
 
-    network_part="$(echo "${ip_bin:0:netmask_length}")"
-    host_length="$((32-netmask_length))"
     if [ "${host_length}" -le 1 ]; then
-        echo 'None'
+        ipfirst="None"
     else
         zeros="$(head -c "$((host_length-1))" /dev/zero | tr '\0' '0')"
         sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${zeros}1")"
-        __bin2ip "${sequence}"
+        ipfirst="$(__bin2ip "${sequence}")"
     fi
 }
 
-# example: 128.42.5.4/21 --> 128.42.7.254 (last usable IP address)
+# example: 128.42.5.4/21 > 128.42.7.254 (last usable IP address)
 __cidr2iplast() {
-    local ip_cidr="${1}"
-    __valid_cidr "${ip_cidr}"
-    local netmask_length="$(echo "${ip_cidr}" | cut -d '/' -f 2)"
-    local ip="$(echo ${ip_cidr} | cut -d '/' -f 1)"
-    local ip_bin="$(__ip2bin "$ip" | tr -d ' ')"
 
-    network_part="$(echo "${ip_bin:0:netmask_length}")"
-    host_length="$((32-netmask_length))"
     if [ "${host_length}" -le 1 ]; then
-        echo 'None'
+        iplast="None"
     else
         ones="$(head -c "$((host_length-1))" /dev/zero | tr '\0' '1')"
         sequence="$(sed -E 's/(.{8})(.{8})(.{8})(.{8})/\1;\2;\3;\4/' <<< "${network_part}${ones}0")"
-        __bin2ip "${sequence}"
+        iplast="$(__bin2ip "${sequence}")"
     fi
 }
 
-__parse_options() {
-    while getopts 'h' option; do
-        case "$option" in
-            h) __print_usage;  exit 0 ;;
-            *) __print_usage;  exit 1 ;;
+__parse_arguments() {
+    args=()
+    while (("$#")); do
+        case "$1" in
+            -h | --help ) __print_usage; exit 0;;
+            -*          ) echo "Invalid option. Exiting..."; exit 1;;
+            *           ) args+=("$1"); shift;;
         esac
     done
 }
 
-__parse_argument() {
-    ip_cidr="${1}"
-}
+__validate_arguments() {
 
-__check_argument() {
+    if [[ "${#args[@]}" -ne 1 ]]; then
+        echo "Number of arguments should be 1. Exiting..."
+        exit 1
+    fi
+
+    ip_cidr="${args[0]}"
+
     __valid_cidr "${ip_cidr}"
 }
 
 __print_tx() {
-    {
-    echo "Network address        : $(__cidr2network   ${ip_cidr})"
-    echo "Broadcast address      : $(__cidr2broadcast ${ip_cidr})"
-    echo "Subnet mask            : $(__cidr2netmask   ${ip_cidr})"
-    echo "First usable address   : $(__cidr2ipfirst   ${ip_cidr})"
-    echo "Last usable address    : $(__cidr2iplast    ${ip_cidr})"
-    echo "Number of usable hosts : $(__cidr2numhosts  ${ip_cidr})"
-    } | column -t -s ':'
+    echo "Network address        : $network"
+    echo "Broadcast address      : $broadcast"
+    echo "Subnet mask            : $netmask"
+    echo "First usable address   : $ipfirst"
+    echo "Last usable address    : $iplast"
+    echo "Number of usable hosts : $numhosts"
 }
 
 __main() {
-    __parse_options "$@"
-    __parse_argument "$@"
-    __check_argument
+    __parse_arguments "$@"
+    __validate_arguments
+
+    __get_ip
+    __get_netmask_length
+    __get_host_length
+    __get_network_part
+
+    __cidr2network
+    __cidr2broadcast
+    __cidr2netmask
+    __cidr2ipfirst
+    __cidr2iplast
+    __cidr2numhosts
+
     __print_tx
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    __main "${@}"
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    __main "$@"
 fi
