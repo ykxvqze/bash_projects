7be499de516d251660832c5f748877e47b8d32bb
ykxvqze <ykxvqz@pm.me>
2025-09-16 11:49:18 +0300

    change flag: keep > remove

--- wrapline.sh
+++ wrapline.sh
@@ -3,15 +3,15 @@
 << 'EOF'
 Create line breaks at a limit of 72 characters without breaking any word
 
-USAGE:  ./cutline.sh [ -r | --rm ] <filename(s)>
+USAGE:  ./wrapline.sh [ -r | --remove ] <filename(s)>
 
 ARGS:
-        input file(s): ASCII text file(s)
+        file(s): ASCII text file(s)
 
 OUTPUT:
         file(s): If -r or --remove switch is specified anywhere, the
                  original file(s) will be overwritten. Otherwise, the
-                 result(s) will be saved as new file(s) with prefix __
+                 result(s) will be saved as new file(s) with a __ prefix
                  i.e. <__filename>, so the original files remain intact.
 
 DESCRIPTION:
@@ -69,64 +69,66 @@ __process_files     () { :; }
 __main              () { :; }
 
 __print_usage() {
-    echo -e "Cut lines at 72 characters without breaking words.
+    echo -e "Wrap lines at 72 characters without breaking words.
 
     Usage:
 
         ./${0##*/} <filename(s)>                    Keep original file(s) intact
         ./${0##*/} [ -r | --remove ] <filename(s)>  Remove original file(s)
-        ./${0##*/} [ -h | --help ]                  Print usage and exit\n"
+        ./${0##*/} [ -h | --help ]                  Print usage and exit \n"
 }
 
 __cut_lines() {
-    local file="${1}"
-    local n_lines="$(cat "$file" | wc -l)"
+    local FILE="${1}"
+    local n_lines="$(cat "$FILE" | wc -l)"
     local i=1
 
-    while [ "$i" -le "$n_lines" ]; do
-        if [ "$(sed -n "$i p" "$file" | wc -c)" -gt 72 ]; then
-            sed -Ei "$i s/^(.{0,72})\s/\1\n/" "$file"
+    while [[ "$i" -le "$n_lines" ]]; do
+        if [[ "$(sed -n "$i p" "$FILE" | wc -c)" -gt 72 ]]; then
+            sed -Ei "$i s/^(.{0,72})\s/\1\n/" "$FILE"
         fi
         let i++
-        n_lines="$(cat "$file" | wc -l)"
+        n_lines="$(cat "$FILE" | wc -l)"
     done
 }
 
 __check_nargs() {
-    if [ "$#" -eq 0 ]; then
+    if [[ "$#" -eq 0 ]]; then
         __print_usage
         exit 1
     fi
 }
 
 __parse_arguments() {
-    # parse and collect filenames in an array
     args=()
-    while [ "$#" -gt 0 ]; do
+    while (("$#")); do
         case "${1}" in
-            -h | --help   ) __print_usage               ; exit 0 ;;
-            -r | --remove ) keep='off'                  ; shift  ;;
-            -*            ) echo "Unknown option: ${1}" ; exit 1 ;;
-             *            ) args+=("${1}")              ; shift  ;;
+            -h | --help   ) __print_usage; exit 0;;
+            -r | --remove ) remove="true"; shift ;;
+            -*            ) echo "Invalid option. Exiting..." ; exit 1;;
+             *            ) args+=("${1}"); shift;;
         esac
     done
 }
 
 __check_files_exist() {
     for f in "${args[@]}"; do
-        [ -f "$f" ] || { echo "file $f does not exist. Exiting ..."; exit 1; }
+        if [[ ! -f "$f" ]]; then
+            echo "File $f does not exist. Exiting..."
+            exit 1
+        fi
     done
 }
 
 __process_files() {
-    if [ "$keep" != 'off' ]; then
+    if [[ "$remove" == "true" ]]; then
         for f in "${args[@]}"; do
-            cp "$f" "$(dirname "$f")/__$(basename "$f")"
-            __cut_lines "$(dirname "$f")/__$(basename "$f")"
+            __cut_lines "$f"
         done
     else
         for f in "${args[@]}"; do
-            __cut_lines "$f"
+            cp "$f" "$(dirname "$f")/__$(basename "$f")"
+            __cut_lines "$(dirname "$f")/__$(basename "$f")"
         done
     fi
 }
@@ -138,6 +140,6 @@ __main() {
     __process_files
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     __main "$@"
 fi
